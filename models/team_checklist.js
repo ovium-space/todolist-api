9ab5cf6b2180c77d28dac5c674c33be7b2a09229
Blackross22 <bigbosskts22@hotmail.com>
2020-11-03 19:54:04 +0700

    :tada:Create team_checklist model

---
+++ models/team_checklist.js
@@ -0,0 +1,48 @@
+module.exports = (sequelize, DataTypes)=>{
+    const team_checklist = sequelize.define("team_checklist", {
+        checklist_ID:{
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNull: false
+        },
+        todolist_ID:{
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        user_ID:{
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        name:{
+            type : DataTypes.STRING,
+            allowNull: false
+        },
+        state:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        checklist_index:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        expire_datetime:{
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        due_datetime:{
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        checklist_check:{
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        }
+    }, {freezeTableName:true, timestamps:false})
+    team_checklist.associate = models =>{
+        team_checklist.belongsTo(models.team_todolist, {foreignKey:"todolist_ID"})
+        team_checklist.belongsToMany(models.user, {through:'Tchecklist_user', foreignKey:"checklist_ID"})
+    }
+
+    return team_checklist
+}
+
--- models/user.js
+++ models/user.js
@@ -32,6 +32,7 @@ module.exports = (sequelize, Datatype) => {
         user.hasMany(models.todolist, {foreignKey: "user_ID"})
         user.belongsToMany(models.team, {through:'team_user', foreignKey:"user_ID"})
         user.hasMany(models.team, {foreignKey:"leader_ID"})
+        user.belongsToMany(models.team_checklist, {through:'Tchecklist_user', foreignKey:"user_ID"})
     }
     return user
 }
