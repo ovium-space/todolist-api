8d82ac8ea7bf98836830e19a652412bc02329d3d
trabnm1313 <jame.sou.com@hotmail.com>
2020-12-23 20:01:53 +0700

    :zap: Add new api
    New api for adding user to team

--- apiRoutes/team.js
+++ apiRoutes/team.js
@@ -42,6 +42,34 @@ router.patch("/update/:id", async (req,res)=>{
     res.send(data)
 })
 
+router.patch("/user/add/:id", async (req, res) => {
+    let teamID = req.params.id
+    let userlist = req.body.userlist
+
+    //Get team data from teamID
+    let teamData = await team.findOne({where:{ team_ID: teamID }}).catch(err => {
+        console.log(err)
+        res.sendStatus(404).send("Team not found.")
+    })
+
+    teamData.addUsers(userlist)
+    res.sendStatus(200)
+})
+
+router.patch("/user/delete/:id", async (req, res) => {
+    let teamID = req.params.id
+    let userlist = req.body.userlist
+
+    //Get team data from teamID
+    let teamData = await team.findOne({where:{ team_ID: teamID }}).catch(err => {
+        console.log(err)
+        res.sendStatus(404).send("Team not found.")
+    })
+
+    teamData.removeUsers(userlist)
+    res.sendStatus(200)
+})
+
 router.delete("/delete/:id", async (req, res)=>{
     let teamID = req.params.id
     let data =  await team.findOne({where: {team_ID: teamID}}).then((result) => {
